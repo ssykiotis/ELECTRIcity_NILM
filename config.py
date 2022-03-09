2e21e6b31fa740b0cd1cbb84167cbfa82e084b92
Stavros Sykiotis <stasykiotis@gmail.com>
2022-03-09 10:08:10 +0200

    added hyperparameters

--- config.py
+++ config.py
@@ -1,5 +1,105 @@
 import argparse
-
+import torch 
+import numpy as np
+import random
 
 def get_args():
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--redd_location',   type = str, default = None)
+    parser.add_argument('--ukdale_location', type = str, default = None)
+
+    parser.add_argument('--seed',            type = int,   default = 0)
+    parser.add_argument('--device',          type = str,   default = 'cpu' ,    choices=['cpu', 'cuda'])
+
+    parser.add_argument('--dataset_code',     type = str,   default = 'uk_dale', choices=['redd_lf', 'uk_dale'])
+    parser.add_argument('--house_indicies',   type = list,  default = [1, 2, 3, 4, 5])
+
+
+    # REDD Dataset appliance names:    'refrigerator', 'washer_dryer',   'microwave','dishwasher'
+    # UK Dale Dataset appliance names: 'fridge',       'washing_machine','microwave','dishwasher','kettle'
+    parser.add_argument('--appliance_names',  type = list,  default = ['washing_machine'])
+
+
+
+
+def setup_seed(seed):
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark     = False  
+    random.seed(seed)                          
+    np.random.seed(seed)                       
+    torch.manual_seed(seed)                    
+    torch.cuda.manual_seed(seed)               
+    torch.cuda.manual_seed_all(seed)   
+
+
+# if args.dataset_code == 'redd_lf':
+#     args.cutoff = {
+#         'aggregate'   : 6000,
+#         'refrigerator': 400,
+#         'washer_dryer': 3500,
+#         'microwave'   : 1800,
+#         'dishwasher'  : 1200
+#     }
+#     args.threshold = {
+#         'refrigerator': 50,
+#         'washer_dryer': 20,
+#         'microwave'   : 200,
+#         'dishwasher'  : 10
+#     }
+#     args.min_on = {
+#         'refrigerator': 10,
+#         'washer_dryer': 300,
+#         'microwave'   : 2,
+#         'dishwasher'  : 300
+#     }
+#     args.min_off = {
+#         'refrigerator': 2,
+#         'washer_dryer': 26,
+#         'microwave'   : 5,
+#         'dishwasher'  : 300
+#     }
+#     args.c0 = {
+#         'refrigerator': 1e-6,
+#         'washer_dryer': 0.001,
+#         'microwave'   : 1.,
+#         'dishwasher'  : 1.
+#     }
+# elif args.dataset_code == 'uk_dale':    
+#     args.cutoff = {
+#         'aggregate'      : 6000,
+#         'kettle'         : 3100,
+#         'fridge'         : 300,
+#         'washing_machine': 2500,
+#         'microwave'      : 3000,
+#         'dishwasher'     : 2500
+#     }
+#     args.threshold = {
+#         'kettle'         : 2000,
+#         'fridge'         : 50,
+#         'washing_machine': 20,
+#         'microwave'      : 200,
+#         'dishwasher'     : 10
+#     }
+#     #multiply by 6 for seconds
+#     args.min_on = {
+#         'kettle'         : 2,
+#         'fridge'         : 10,
+#         'washing_machine': 300,
+#         'microwave'      : 2,
+#         'dishwasher'     : 300
+#     }
+#     #multiply by 6 for seconds
+#     args.min_off = {
+#         'kettle'         : 0,
+#         'fridge'         : 2,
+#         'washing_machine': 26,
+#         'microwave'      : 5,
+#         'dishwasher'     : 300
+#     }
+#     args.c0 = {
+#         'kettle'         : 1.,
+#         'fridge'         : 1e-6,
+#         'washing_machine': 0.01,
+#         'microwave'      : 1.,
+#         'dishwasher'     : 1.
+#     }
--- electricity.py
+++ electricity.py
@@ -4,69 +4,71 @@ from config import *
 if __name__ == "__main__":
 
     args = get_args()
+    setup_seed(args.seed)
 
-    if args.dataset_code == 'redd_lf':
-        args.house_indicies = [2, 3, 4, 5, 6]
-        ds_parser = REDD_Parser(args)
-    elif args.dataset_code == 'uk_dale':
-        args.house_indicies = [1, 3, 4, 5]
-        ds_parser = UKDALE_Parser(args)
 
-    model = ELECTRICITY(args)
-    trainer = Trainer(args,model,stats) #is stats necessary?
+    # if args.dataset_code == 'redd_lf':
+    #     args.house_indicies = [2, 3, 4, 5, 6]
+    #     ds_parser = REDD_Parser(args)
+    # elif args.dataset_code == 'uk_dale':
+    #     args.house_indicies = [1, 3, 4, 5]
+    #     ds_parser = UKDALE_Parser(args)
 
-    #Training Loop
-    start_time = time()
-    if args.num_epochs > 0:
-    try:
-        model.load_state_dict(torch.load(os.path.join(
-            args.export_root, 'best_acc_model.pth'), map_location='cpu'))
-        print('Successfully loaded previous model, continue training...')
-    except FileNotFoundError:
-        print('Failed to load old model, continue training new model...')
-    trainer.train()
+    # model = ELECTRICITY(args)
+    # trainer = Trainer(args,model,stats) #is stats necessary?
 
-    end_time = time()
+    # #Training Loop
+    # start_time = time()
+    # if args.num_epochs > 0:
+    # try:
+    #     model.load_state_dict(torch.load(os.path.join(
+    #         args.export_root, 'best_acc_model.pth'), map_location='cpu'))
+    #     print('Successfully loaded previous model, continue training...')
+    # except FileNotFoundError:
+    #     print('Failed to load old model, continue training new model...')
+    # trainer.train()
 
-    training_time = end_time-start_time
-    print("Total Training Time: " + str(training_time/60) + "minutes")
+    # end_time = time()
 
-    #Testing Loop
-    args.validation_size = 1.
-    if args.dataset_code == 'redd_lf':
-        args.house_indicies = [1]
-        dataset = REDD_LF_Dataset(args, stats)
-    elif args.dataset_code == 'uk_dale':
-        args.house_indicies = [2]
-        dataset = UK_DALE_Dataset(args, stats)
+    # training_time = end_time-start_time
+    # print("Total Training Time: " + str(training_time/60) + "minutes")
 
-    dataloader = NILMDataloader(args, dataset)
-    _, test_loader = dataloader.get_dataloaders()
-    rel_err, abs_err, acc, prec, recall, f1 = trainer.test(test_loader)
-    print('Mean Accuracy:', acc)
-    print('Mean F1-Score:', f1)
-    print('Mean Relative Error:', rel_err)
-    print('Mean Absolute Error:', abs_err)
+    # #Testing Loop
+    # args.validation_size = 1.
+    # if args.dataset_code == 'redd_lf':
+    #     args.house_indicies = [1]
+    #     dataset = REDD_LF_Dataset(args, stats)
+    # elif args.dataset_code == 'uk_dale':
+    #     args.house_indicies = [2]
+    #     dataset = UK_DALE_Dataset(args, stats)
 
-    results = dict()
+    # dataloader = NILMDataloader(args, dataset)
+    # _, test_loader = dataloader.get_dataloaders()
+    # rel_err, abs_err, acc, prec, recall, f1 = trainer.test(test_loader)
+    # print('Mean Accuracy:', acc)
+    # print('Mean F1-Score:', f1)
+    # print('Mean Relative Error:', rel_err)
+    # print('Mean Absolute Error:', abs_err)
 
-    results['args']          = args
-    results['training_time']  = training_time/60
-    results['best_epoch']    = trainer.best_model_epoch 
-    results['training_loss'] = trainer.training_loss
-    results['val_rel_err']   = trainer.val_rel_err
-    results['val_abs_err']   = trainer.val_abs_err
-    results['val_acc']       = trainer.val_acc
-    results['val_precision'] = trainer.val_precision
-    results['val_recall']    = trainer.val_recall
-    results['val_f1']        = trainer.val_f1
+    # results = dict()
 
-    results['label_curve']   = trainer.label_curve
-    results['e_pred_curve']  = trainer.e_pred_curve
-    results['status_curve']  = trainer.status_curce
-    results['s_pred_curve']  = trainer.s_pred_curve
+    # results['args']          = args
+    # results['training_time']  = training_time/60
+    # results['best_epoch']    = trainer.best_model_epoch 
+    # results['training_loss'] = trainer.training_loss
+    # results['val_rel_err']   = trainer.val_rel_err
+    # results['val_abs_err']   = trainer.val_abs_err
+    # results['val_acc']       = trainer.val_acc
+    # results['val_precision'] = trainer.val_precision
+    # results['val_recall']    = trainer.val_recall
+    # results['val_f1']        = trainer.val_f1
 
-    import pickle as pkl
-    fname = Path(args.export_root).joinpath('results.pkl')
-    pkl.dump(results,open( fname, "wb" )) 
+    # results['label_curve']   = trainer.label_curve
+    # results['e_pred_curve']  = trainer.e_pred_curve
+    # results['status_curve']  = trainer.status_curce
+    # results['s_pred_curve']  = trainer.s_pred_curve
+
+    # import pickle as pkl
+    # fname = Path(args.export_root).joinpath('results.pkl')
+    # pkl.dump(results,open( fname, "wb" )) 
 
